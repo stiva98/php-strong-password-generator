2353a615002aa99df7844fedddff2699d63b2b87
stiva98 <samustiva9810@gmail.com>
2023-08-23 16:54:26 +0200

    add milestone 1

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+    function generateRandomPassword($length) {
+        $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+[]{}|;:,.<>?';
+        $password = '';
+        
+        for ($i = 0; $i < $length; $i++) {
+            $randomChar = $chars[rand(0, strlen($chars) - 1)];
+            $password .= $randomChar;
+        }
+        
+        return $password;
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+        <title>PHP Strong Password Generator</title>
+    </head>
+<body>
+    <header>
+        <h1>
+            PHP Strong Password Generator
+        </h1>
+    </header>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <form action="index.php" method="GEt">
+                        <label for="inputPassword5" class="form-label">Lunghezza password:</label>
+                        <input type="number" min="1" id="inputPassword5" class="form-control" name="passwordLength" required>
+                        <div class="col-auto">
+                            <button type="submit" class="btn btn-primary mt-3">Invia</button>
+                        </div>
+                    </form>
+                    <?php 
+                    if (isset($_GET['passwordLength'])) {
+                        $passwordLength = $_GET['passwordLength'];
+                        $randomPassword = generateRandomPassword($passwordLength);
+                    ?>
+                    <p>
+                        Password casuale generata: <strong><?php echo "$randomPassword"; ?></strong>
+                    </p>
+                    <?php 
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </main>
+    
+</body>
+</html>
